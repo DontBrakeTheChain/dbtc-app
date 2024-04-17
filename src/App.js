2f68cd87e6763bdfe0544238788c6a9ed6369636
eektheeek <larionov.eek@gmail.com>
2024-04-17 17:53:16 +0300

    remove useless files

--- src/App.js
+++ src/App.js
@@ -11,15 +11,6 @@ export default function App() {
 
   return (
     <div className="ui-container">
-      {buttonStatus ? (
-        <>
-          <h2 className="ui-title">Welcome to the "Don't break the chain" calendar!</h2>
-          <div class="img"/>
-          <button onClick={handleStatusChange} className="ui-button">
-            Log in
-          </button>
-        </>
-      ) : (
         <>
           <CalendarPage />
           <button onClick={handleStatusChange} className="ui-button logout">
@@ -27,7 +18,6 @@ export default function App() {
           </button>
           
         </>
-      )}
     </div>
   );
 }
